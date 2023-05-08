3c41252991459bf93d47a3ed6c130e961c706c94
yodhcn <47470844+yodhcn@users.noreply.github.com>
2023-05-08 14:14:27 +0800

    12.11. Storing the Image on the Filesystem
    https://docs.expo.dev/versions/latest/sdk/filesystem/

--- store/createPlacesSlice.js
+++ store/createPlacesSlice.js
@@ -1,9 +1,23 @@
 import Place from "../models/place";
+import * as FileSystem from "expo-file-system";
 
 export const createPlacesSlice = (set) => ({
   places: [],
-  addPlace: (title, imageUri) => {
-    const newPlace = new Place(new Date().toString(), title, imageUri);
+  addPlace: async (title, imageUri) => {
+    const fileName = imageUri.split("/").pop();
+    const newPath = FileSystem.documentDirectory + fileName;
+
+    try {
+      await FileSystem.moveAsync({
+        from: imageUri,
+        to: newPath,
+      });
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+
+    const newPlace = new Place(new Date().toString(), title, newPath);
     set((state) => {
       state.places.push(newPlace);
     });
