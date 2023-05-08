06c2cddc6dc74e9f5a4a22492258d302f952c0f6
yodhcn <47470844+yodhcn@users.noreply.github.com>
2023-05-08 14:57:29 +0800

    12.12. Diving into SQLite for Permanent Data Storage
    https://docs.expo.dev/versions/latest/sdk/sqlite/

--- App.js
+++ App.js
@@ -2,6 +2,16 @@ import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 
 import PlacesNavigator from "./navigation/PlacesNavigator";
+import { init } from "./helpers/db";
+
+init()
+  .then(() => {
+    console.log("Initialized database");
+  })
+  .catch((err) => {
+    console.log("Initializing db failed.");
+    console.log(err);
+  });
 
 export default function App() {
   return (
---
+++ helpers/db.js
@@ -0,0 +1,24 @@
+import * as SQLite from "expo-sqlite";
+
+const db = SQLite.openDatabase("places.db");
+
+export const init = () => {
+  const promise = new Promise((resolve, reject) => {
+    // https://docs.expo.dev/versions/latest/sdk/sqlite/#sqltransaction
+    db.transaction((tx) => {
+      // lat -> latitude
+      // lng -> longitude
+      tx.executeSql(
+        "CREATE TABLE IF NOT EXISTS places (id INTEGER PRIMARY KEY NOT NULL, title TEXT NOT NULL, imageUri TEXT NOT NULL, address TEXT NOT NULL, lat REAL NOT NULL, lng REAL NOT NULL);",
+        [],
+        () => {
+          resolve();
+        },
+        (_, err) => {
+          reject(err);
+        }
+      );
+    });
+  });
+  return promise;
+};
